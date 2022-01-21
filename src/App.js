b55f4cb9dfaed8f8d8deb9cea718faca16195084
Castillo Abel <castillokev.abel@gmail.com>
2022-01-21 18:26:51 -0300

    Pagina Creada

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import { useState, useEffect, useReducer } from "react"
 import "./App.css"
-// const useProductos = inicial=>{
-//   const [productos, setProductos] = useState(inicial)
-//   const sumar = (productos)=> {
-
-//     setProductos(productos + 1)
-//   }
-// return [productos, sumar]
-// }
-const ButtonCompra = ({id, productos, sumar, dispatch, name})=>{
+const ButtonCompra = ({id, productos, sumar, dispatch, name, key})=>{
   return(
     <button
       id={id} 
       className={"button"}
       onClick={()=>{
-        sumar(productos+1)
+        sumar(()=>productos + 1)
         dispatch({type: name})
-      }}
+      }
+    }
       >Añadir al Carro
     </button>
   )
+
 }
-const Compra = ({id, className, price, name, productos, sumar, dispatch})=>{
+const Compra = ({id, className, price, name, productos, sumar, dispatch, key})=>{
   return(
       <div 
         id={id} 
@@ -46,6 +40,7 @@ const Compra = ({id, className, price, name, productos, sumar, dispatch})=>{
               id={`button-${id}`}
               dispatch={dispatch}
               name={id}
+
               >
 
             </ButtonCompra>
@@ -103,14 +98,14 @@ if (cant > 0) return (
   )
 return null
 }
-const Carro = ({productos, sumar, dispatch})=>{
+const Carro = ({productos, sumar, dispatch, state, click, setClick})=>{
   return (
     <div id="button-cont">
       <Aviso cant={productos}></Aviso>
       <button 
         className={"button"}
        id={"compra"}
-       
+       onClick={()=>setClick(!click)}
        >Ver Carro
 
        </button>
@@ -118,49 +113,109 @@ const Carro = ({productos, sumar, dispatch})=>{
     
   )
 }
-const Menu = ()=>{
+const Menu = (state, click, setClick, productos)=>{
+  const [lechuga, tomate, arveja] = [...state.state]
   return(
-    <div id="men">
-
+    <div id={"men"}>
+      <ul
+      id={"lista"}
+      >
+        <li className="pro"><p className="n">Lechuga</p>   
+          <img
+            id={`i${lechuga.name}`}
+          ></img>
+          <span className="espan">{lechuga.cant}</span>
+        </li>
+        <li className="pro"><p className="n">Tomate</p>   
+          <img
+            id={`i${tomate.name}`}
+          ></img>
+        <span className="espan"> {tomate.cant}</span>
+        </li>
+        <li className="pro"><p className="n">Arveja</p> 
+          <img
+            id={`i${arveja.name}`}
+          ></img>
+          <span className="espan">{arveja.cant}</span>
+        </li>
+      </ul>
     </div>
   )
 }
-const Navbar = ({productos, sumar, dispatch, state})=>{
+const Navbar = ({productos, sumar, dispatch, state, click, setClick})=>{
   return (
     <div className="barra">
       <Titulo name={"Tienda"}></Titulo>
-      <Carro productos={productos} sumar={sumar} dispatch={dispatch}></Carro>
-      <Menu state={state}></Menu>
+      <Carro 
+        productos={productos}
+        sumar={sumar} 
+        dispatch={dispatch}
+        click={click}
+        setClick={setClick}
+        >
+
+        </Carro>
+      <Menu 
+      state={state}
+      click={click}
+      setClick={setClick}
+      productos={productos}
+      >
+
+      </Menu>
     </div>
   )
 }
-const initial = {
-  verdura: "",
-  img: "",
+// const initial = {
+//   verdura: "",
+//   img: "",
+//   cant: 0
+// }
+
+const initial =[
+{
+  name:"lechuga",
+  img: "lechuga.jpg",
   cant: 0
-}
+},
+{
+  name:"tomate",
+  img: "tomate.jpg",
+  cant: 0
+},
+{
+  name:"arveja",
+  img: "arbeja.jpg",
+  cant: 0
+}]
 // const state = {verdura: string}
 // action ={type:string, payload: any}
 // 
 const reducer = (state, action)=>{
 switch (action.type){
   case "lechuga":
-    return {verdura: "lechuga", img: "lechuga.jpg", cant: 1}
+    return [{...state[0], cant: state[0].cant+1}, state[1], state[2]]
   case "tomate":
-    return {verdura: "tomate", img: "tomate.jpg", cant: 1}
+    return [state[0], {...state[1], cant: state[1].cant+1}, state[2]]
   case "arveja":
-    return {verdura: "arveja", img: "arbejas.jpg", cant: 1}
+    return [state[0], state[1], {...state[2], cant: state[2].cant+1}]
   default:
     return state
 }
 }
 const App = ()=>{
   const [productos, setProductos] = useState(0)
+  const [click, setClick] = useState(false)
   const [state, dispatch] = useReducer(reducer, initial)
   useEffect(()=>{
-    console.log("que compraste?")
-    console.log(state)
   }, [productos])
+  useEffect(()=>{
+    if (!click) document.getElementById("men").style.opacity = 0
+    else document.getElementById("men").style.opacity = 1
+  }, [click])
+  useEffect(()=>{
+
+  }, [state])
   return (
     <div 
     className={"conteiner"}
@@ -170,6 +225,8 @@ const App = ()=>{
         sumar={setProductos}
         dispatch={dispatch}
         state={state}
+        click={click}
+        setClick={setClick}
         >
       </Navbar>
       <Catalogo 
